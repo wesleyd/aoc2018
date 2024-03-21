0baf54b8c418934975b45cfbdd47a9f7bc1c3845
Wesley Darlington <wesley.darlington@gmail.com>
2024-03-20 20:31:03 -0400

    day 11 - fast enough, but with a hilarious off-by-one, although compensated for

--- day11b.py
+++ day11b.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import math
 
 def power(serial, x, y):
@@ -19,31 +18,29 @@ assert (got := power(71, 101, 153)) == 4, got
 class Grid:
     def __init__(self, serial):
         self.serial = serial
-
-    @functools.lru_cache(maxsize=300*300*10)
-    def power(self, x, y):
-        if x < 0 or 300 <= x or y < 0 or 300 <= y:
-            return 0
-        return power(self.serial, x, y)
-
-    @functools.lru_cache(maxsize=300*300*300*10)
+        self.power = [[0 for x in range(300)] for y in range(300)]
+        for x in range(300):
+            for y in range(300):
+                self.power[x][y] = power(self.serial, x, y)
+        self.area = [[0 for x in range(300)] for y in range(300)]
+        self.area[0][0] = self.power[0][0]
+        for n in range(1, 300):
+            self.area[0][n] = self.area[0][n-1] + self.power[0][n]
+            self.area[n][0] = self.area[n-1][0] + self.power[n][0]
+        for x in range(1, 300):
+            for y in range(1, 300):
+                self.area[x][y] = (self.area[x-1][y]
+                                +  self.area[x][y-1]
+                                -  self.area[x-1][y-1]
+                                +  self.power[x][y])
+            
     def powerSQ(self, tlx, tly, sq):
-        #print(f'power3x3 {tlx=} {tly=} {sq=}')
-        assert sq > 0, (tlx, tly, sq)
         if sq == 1:
-            return self.power(tlx, tly)
-        pwr = 0
-        half = sq//2
-        pwr += self.powerSQ(tlx,      tly,      half)
-        pwr += self.powerSQ(tlx+half, tly,      half)
-        pwr += self.powerSQ(tlx,      tly+half, half)
-        pwr += self.powerSQ(tlx+half, tly+half, half)
-        if sq % 2 == 1:
-            for n in range(sq-1):
-                pwr += self.power(tlx+n, tly+sq-1)
-                pwr += self.power(tlx+sq-1, tly+n)
-            pwr += self.power(tlx+sq-1, tly+sq-1)
-        return pwr
+            return self.power[tlx][tly]
+        return (self.area[tlx+sq-1][tly+sq-1]
+              + self.area[tlx]     [tly]
+              - self.area[tlx+sq-1][tly]
+              - self.area[tlx]     [tly+sq-1])
 
     def max_power(self):
         mp = -math.inf
@@ -56,11 +53,10 @@ class Grid:
                         mp = p
                         mx, my = x, y
                         msq = sq
-                        print(f'grid={self.serial},{mx,my,msq},{mp=}')
-        return mx, my, msq
+        return mx+1, my+1, msq-1
 
-#assert (got := Grid(18).max_power()) == (90, 269, 16), got
-#assert (got := Grid(42).max_power()) == (232, 251, 12), got
+assert (got := Grid(18).max_power()) == (90, 269, 16), got
+assert (got := Grid(42).max_power()) == (232, 251, 12), got
 
 with open('inputs/day11.input.txt') as f:
     real_input = f.read()
